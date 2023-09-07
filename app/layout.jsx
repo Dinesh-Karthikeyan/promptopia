8cb30086c72e7f7bd0a4b8de6e604fcdb0cac961
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-09-07 13:26:39 +0530

    added a basic layout.jsx with metadata

---
+++ app/layout.jsx
@@ -0,0 +1,14 @@
+import '@styles/globals.css'
+
+export const metadata = {
+    title: "Promptopia",
+    description: "Discover and share AI prompts",
+}
+
+const layout = () => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default layout
