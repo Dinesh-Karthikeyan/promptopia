d953f99814abf95e4e0cb72c5972e09c7a14b040
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-09-07 13:25:24 +0530

    modified tailwind.config.js content

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,11 @@ module.exports = {
     extend: {
       fontFamily: {
         satoshi: ['Satoshi', 'sans-serif'],
-        inter: ["Inter", 'sans-serif']
+        inter: ['Inter', 'sans-serif'],
       },
       colors: {
         'primary-orange': '#FF5722',
-      },
+      }
     },
   },
   plugins: [],
