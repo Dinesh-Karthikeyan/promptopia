c499ba431a5c2592553fc6a1d327d4804bf6e703
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-09-07 13:40:57 +0530

    added necessary layout tags to the layout file

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,24 @@
 import '@styles/globals.css'
+import { Children } from 'react'
 
 export const metadata = {
     title: "Promptopia",
     description: "Discover and share AI prompts",
 }
 
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
+const RootLayout = ({children}) => {
+    return (
+        <html lang='en'>
+            <body>
+                <div className='main'>
+                    <div className='gradient'/>
+                </div>  
+                <main className='app'>
+                    {children}
+                </main>
+            </body>
+        </html>
+    )
 }
 
-export default layout
+export default RootLayout
