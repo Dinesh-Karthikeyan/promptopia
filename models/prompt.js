34f4327b6e2d1ca56a39b729d1db116eacd84e84
K Dinesh Karthikeyan <dinuvedi@gmail.com>
2023-09-18 18:37:44 +0530

    created a model to create and fetch prompts from the database

--- models/prompt.js
+++ models/prompt.js
@@ -1,20 +1,20 @@
-import {Schema, model, models} from 'mongoose'
+import { Schema, model, models } from "mongoose";
 
 const promptSchema = new Schema({
-    creator: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    },
-    prompt: {
-        type:String,
-        required: [true, "A Prompt message is required"]
-    },
-    tag: {
-        type:String,
-        required: [true, "Tag is neccesary to show prompt in search"]
-    }
-})
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  prompt: {
+    type: String,
+    required: [true, "A Prompt message is required"],
+  },
+  tag: {
+    type: String,
+    required: [true, "Tag is neccesary to show prompt in search"],
+  },
+});
 
-const Prompt = models.Prompt || model("Prompt", promptSchema)
+const Prompt = models.Prompt || model("Prompt", promptSchema);
 
-export default Prompt
+export default Prompt;
